47a7df2da5b3122c853c65b472011d1b1d18f4af
Patrick Grasseels <grasseelsp@hotmail.com>
2021-12-14 11:08:21 +0100

    Push base failure repository project

---
+++ src/MAUIWebAuthenticator/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace MAUIWebAuthenticator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartAuthenticateAsync(object sender, EventArgs e)
+        {
+            var authResult = await WebAuthenticator.AuthenticateAsync(
+                            new Uri("https://mysuperurl.dotnet"),
+                            new Uri("mysuperdomain://"));
+
+        }
+    }
+}
